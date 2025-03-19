bd0e2efc05ac4d4026a8f3a759560e3a22a651c6
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-03-19 23:10:33 +0530

    cc

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const express = require("express");
+const mongoose = require("mongoose");
+const gameRoutes = require("./routes/gameRoutes");
+const app = express();
+
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect("mongodb://localhost:27017/antakshari", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.log(err));
+
+// API Routes
+app.use("/api/game", gameRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
