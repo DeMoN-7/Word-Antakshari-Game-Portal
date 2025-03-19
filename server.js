125c5c6e873632e9188a91c75cdc71eb2f718cce
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2025-03-19 23:28:57 +0530

    cc

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-// server.js
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Import CORS
+
 const gameRoutes = require("./routes/gameRoutes");
+
 const app = express();
 
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 
 // MongoDB Connection
 mongoose
   .connect("mongodb://localhost:27017/antakshari")
   .then(() => console.log("MongoDB Connected!"))
   .catch((err) => console.log(err));
-
 
 // API Routes
 app.use("/api/game", gameRoutes);
